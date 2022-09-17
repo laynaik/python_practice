cff348b4469f481f3e749fd262571e035d87f893
laynaik <laynaik13@gmail.com>
2022-09-17 15:18:20 +0530

    python practice

---
+++ oop8.py
@@ -0,0 +1,13 @@
+#dtermine instance of class
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+print(isinstance(School_bus,Bus))
+print(isinstance(School_bus,Vehicle))
