eaa0e9db0471e2ee1e0ecb5703e05e565fb85022
laynaik <laynaik13@gmail.com>
2022-09-17 12:36:12 +0530

    Python practice

--- oop1.py
+++ oop1.py
@@ -1,23 +1,23 @@
 
 
 
-# class Dog():
-#     #class attribute remains same for all 
-#     species="junglee"
-#     def __init__(self,name,breed):
-#         self.breed = breed
-#         self.name = name
-#     #methods (functions acting on object )
-#     #operation/ action the are function inside class 
-#     #attributes dont have() while methods have this  
-#     def bark(self,number):
-#         print("bhau bahu my name is {} the number is {}".format(self.name,number)) 
+class Dog():
+    #class attribute remains same for all 
+    species="junglee"
+    def __init__(self,name,breed):
+        self.breed = breed
+        self.name = name
+    #methods (functions acting on object )
+    #operation/ action the are function inside class 
+    #attributes dont have() while methods have this  
+    def bark(self,number):
+        print("bhau bahu my name is {} the number is {}".format(self.name,number)) 
 
 
 
-# my_dog=Dog('vismay','stray')
-# print(my_dog.name)
-# my_dog.bark(100)
+my_dog=Dog('vismay','stray')
+print(my_dog.name)
+my_dog.bark(100)
 
 from cmath import pi
 
