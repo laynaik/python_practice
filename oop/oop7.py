a43281c17e4ec5e83f083bf1605af4a9d4fe26c3
laynaik <laynaik13@gmail.com>
2022-09-17 15:09:41 +0530

    python practice

---
+++ oop/oop7.py
@@ -0,0 +1,14 @@
+#type of object
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+    def fare(self):
+        return self.capacity * 100
+
+class Bus(Vehicle):
+    pass
+vovlo=Bus('bacardi',100,10)
+print(type(vovlo))
